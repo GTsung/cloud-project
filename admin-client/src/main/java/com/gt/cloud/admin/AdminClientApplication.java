42aa10fac6a348d691fd402a51efbd84dc5f90ab
GTsung <xc_goo@163.com>
2022-01-24 00:48:45 +0800

    spring-admin

---
+++ admin-client/src/main/java/com/gt/cloud/admin/AdminClientApplication.java
@@ -0,0 +1,19 @@
+package com.gt.cloud.admin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+/**
+ * @author GTsung
+ * @date 2022/1/24
+ */
+@EnableDiscoveryClient
+@SpringBootApplication
+public class AdminClientApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AdminClientApplication.class, args);
+    }
+
+}
---
+++ admin-server/src/main/java/com/gt/cloud/admin/AdminServerApplication.java
@@ -0,0 +1,20 @@
+package com.gt.cloud.admin;
+
+import de.codecentric.boot.admin.server.config.EnableAdminServer;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+/**
+ * @author GTsung
+ * @date 2022/1/23
+ */
+@EnableDiscoveryClient
+@EnableAdminServer
+@SpringBootApplication
+public class AdminServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AdminServerApplication.class, args);
+    }
+}
