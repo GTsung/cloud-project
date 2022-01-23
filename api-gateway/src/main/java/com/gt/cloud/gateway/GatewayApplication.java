6be2ce57e5e539c1db939ff90e18bde995d55b5d
GTsung <xc_goo@163.com>
2022-01-23 23:21:05 +0800

    gateway

---
+++ api-gateway/src/main/java/com/gt/cloud/gateway/GatewayApplication.java
@@ -0,0 +1,20 @@
+package com.gt.cloud.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+/**
+ * @author GTsung
+ * @date 2022/1/23
+ */
+
+@EnableDiscoveryClient
+@SpringBootApplication
+public class GatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GatewayApplication.class, args);
+    }
+
+}
